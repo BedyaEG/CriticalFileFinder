66cdcb714b0a30af7b089181dd4de20ac2d9ec3a
BedyaEG <41299181+BedyaEG@users.noreply.github.com>
2018-09-11 14:21:56 +0400

    Add files via upload

---
+++ cores/Attacker.py
@@ -0,0 +1,25 @@
+import requests
+import time
+
+def dirb():
+    print("[+] Please Read README.MD First Necessary !  [+] ")
+    url_in = input("Enter The Site To Search  ==> ")
+    dir_dict = input("Enter Your Directory Dictionary List >> ")
+    dir_dicts = open(dir_dict,'r').readlines()
+    for items in dir_dicts:
+        fu = items.strip()
+        headers = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0"}
+        url = url_in+"/"+fu
+        request = requests.get(url,headers=headers)
+        http = request.content
+        
+        #if b'not found on this server' or b'Error 404 (Not Found)!!1' or b'Error 404' or b'Sh*t I Cant Find It' or b'Sorry ' or b'Not Found' or b'Ops File Not Found' or b'File Not Found On That Server' or b'doesn/t exists' or b'something went wrong':
+            #print("File Not Found On The Server >> "+fu)
+        if request.status_code >= 404 and b'404' or b'Not Found'  in http:
+            print("File Not Found On The Server ==>  "+fu)
+        else:
+            print("File Has Been Found Successfully >> "+fu)
+            fo = open('Critical_File_Found.txt','a')
+            fo.write(url)
+            fo.close()
+dirb()
---
+++ cores/Tool_Info.py
@@ -0,0 +1,7 @@
+import time
+def tool_info():
+    print("""
+       Tool Based on Searching Common Files Using Dictionary To Find Criticals Files On The Server \n Which Mean That Will Help The Bug Hunters To Find The Files More Quicker ! \n Use It At Your Own Risk \n @BedyaEG
+        """)
+    time.sleep(2)
+tool_info()
